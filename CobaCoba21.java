815913e012f433f6965730c87dc590a2aee5ee54
Tiara-Diba <80020178+Tiara-Diba@users.noreply.github.com>
2021-03-19 21:38:45 +0700

    Add files via upload

---
+++ CobaCoba21.java
@@ -0,0 +1,17 @@
+//Fig.2.1:Welcome1. java
+//Text-printing program.
+package coba.coba.pkg2.pkg1;
+
+
+public class CobaCoba21 {
+    {
+        //main method begins execution of Java application
+    }
+   
+    public static void main(String[] args) 
+    {
+     System.out.print("welcome to ");
+     System.out.println("Welcome to Java Programming! ");
+    }
+    
+}
